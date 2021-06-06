b3565e2fa83fde22e2ca1128eaeab9096f77c05b
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-06-06 18:09:44 -0300

    error component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,15 +42,18 @@ export default function Home(): JSX.Element {
   return (
     <>
     {isLoading? (<Loading />):(
-      <>
-        <Header />
-        <Box maxW={1120} px={20} mx="auto" my={20}>
-          <CardList cards={formattedData} />
-          {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-        </Box>
-      </>
+      isError? (<Error />):(
+        <>
+          <Header />
+          <Box maxW={1120} px={20} mx="auto" my={20}>
+            <CardList cards={formattedData} />
+            {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+          </Box>
+        </>
+      )
     )}
 
     </>
   );
 }
+
